a5c1967f52dd4b9cdc8afae6bae3978821e05454
sree-bh <sritama.imps@gmail.com>
2015-11-16 15:59:20 +0530

    removed comments

--- app/viewer/snDirective.js
+++ app/viewer/snDirective.js
@@ -5,18 +5,6 @@ define('snDir', ['angular'], function (ng) {
 	    .module('sn-widget', [])
 	    .directive('snapViewer', ['$timeout', '$interval', snViewer]);
 
-//	function snViewer($timeout) {
-//		var directive = {
-//	        link: link,
-//	        template: '<div style="background-image: url({{item.path}});" class="slide"></div>',
-//	        restrict: 'EAC'
-//	    };
-//
-//		return directive;
-//
-//	    function link(scope, el, attrs) {
-//	    }
-//	}
 	function snViewer($timeout, $interval) {
 		var lastSnap,
 	    	ssInterval,
